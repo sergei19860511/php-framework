52a73962cdbcd230de2fe737e442811d067f7ecf
sergei <s.kupriyanov@nextcontact.ru>
2024-01-09 21:48:34 +0300

    добавил класс Response

--- public/index.php
+++ public/index.php
@@ -3,5 +3,10 @@
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 use Sergei\PhpFramework\Http\Request;
+use Sergei\PhpFramework\Http\Response;
 
-dd(Request::createFromGlobals());
+$request = Request::createFromGlobals();
+
+$content = 'Hello World!!!';
+$response = new Response($content, 200, []);
+$response->send();
