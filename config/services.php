bcce5910d1fdfa773cb9a90d1ad9c3954f2900ec
sergei <s.kupriyanov@nextcontact.ru>
2024-01-16 21:37:47 +0300

    добавляем сервисы в контейнер

--- config/services.php
+++ config/services.php
@@ -1,5 +1,14 @@
 <?php
 
 use League\Container\Container;
+use Sergei\PhpFramework\Http\Kernel;
+use Sergei\PhpFramework\Routing\Router;
+use Sergei\PhpFramework\Routing\RouterInterface;
 
-return new Container();
+$container = new Container();
+
+$container->add(RouterInterface::class, Router::class);
+
+$container->add(Kernel::class)->addArgument(RouterInterface::class);
+
+return $container;
